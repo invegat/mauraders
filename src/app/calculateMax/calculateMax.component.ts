a0b559003a97532767fe6e7b1649c2bf977ea7b9
invegat <mark.c.oliver@gmail.com>
2017-06-13 12:38:30 -0400

    add correct scout button

--- src/app/calculateMax/calculateMax.component.ts
+++ src/app/calculateMax/calculateMax.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  ReactiveFormsModule, FormControl, FormBuilder,
+  ReactiveFormsModule, FormControl, FormBuilder, FormsModule,
   FormGroup, Validators
 } from '@angular/forms';
-//import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+// import { MdButtonModule, MdCheckboxModule } from '@angular/material';
 import { maxValueValidator } from './maxValueValidator';
+import { INumberHash } from '../../utils/iDictionary';
 
 @Component({
   selector: 'calculateMax',
@@ -16,12 +17,16 @@ export class CalculateMaxComponent implements OnInit {
   public submitted = false;
   public active = false;
   public scoutIcon: string;
+  public useRum = 'Use Rum as most accurate Scout';
+  public useGl = 'Use maximum of Gold and Lumber as most accurate Scout';
+  public goodScout: string = this.useRum;
   public pregex = new RegExp('^\\d{1,7}$');
   public mregex = new RegExp('^\\d{1,6}$');
   public pValidators = [Validators.required, Validators.pattern(this.pregex),
-      Validators.minLength(1), Validators.maxLength(7), maxValueValidator(1500000)];
+  Validators.minLength(1), Validators.maxLength(7), maxValueValidator(1500000)];
   public mValidators = [Validators.required, Validators.pattern(this.mregex),
-      Validators.minLength(1), Validators.maxLength(6), maxValueValidator(100000)];
+  Validators.minLength(1), Validators.maxLength(6), maxValueValidator(100000)];
+
   public formErrors = {
     sgold: '',
     slumber: '',
@@ -50,7 +55,32 @@ export class CalculateMaxComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     console.log('calculateMax constructor');
   }
+  /*
+A7 is raid gL
+A8 is raid rum
+B7 is scout gL
+B8 is scout rum
+A9 is max raid 
 
+
+  */
+  private calculateMax(r: [INumberHash]): number {
+    let useRum = (this.goodScout == this.useRum);
+    let rv: number = 0;
+
+    if (useRum) {
+// =(B7 * 2) - ((A9*(B8/2)/A8) - (B8/2))
+      let maxGl = Math.max(r['gold'],r['lumber']); // A7
+      let smaxGl = Math.max(r['sgold'],r['slumber']); // B7
+      rv = (smaxGl * 2) - ((r['maxRaidTotal'] * (r['srum']/2) / r['rum'])-(r['srum']/2)) 
+
+
+    }
+    return 0;
+  }
+  public setGoodScout(event) {
+    this.goodScout = (this.goodScout == this.useRum) ? this.useGl : this.useRum;
+  }
   public buildForm(): void {
     console.log('in buildForm');
     this.formModel = this.fb.group({
@@ -73,12 +103,39 @@ export class CalculateMaxComponent implements OnInit {
   }
   public onSubmit() {
     this.submitted = true;
-    this.onValueChanged(this.formModel);
+    let resources = this.onValueChanged(this.formModel);
     console.log('submit this.formModel.value', this.formModel.value);
     console.log('submit this.formModel.errors', this.formModel.errors);
   }
-  public onValueChanged(data?: any) {
+  public anyFormErrors(): boolean {
+    let resourceEntities: [INumberHash] = [{gold: 0}];
     if (!this.formModel) { return; }
+    const form = this.formModel;
+    for (const field in this.formErrors) {
+      if (field.length === 0) {
+        continue;
+      }
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        console.log('anyFormErrors returning true');
+        console.log('error field:', field);
+        console.log('error formControl :', control);
+        return true;
+      }
+      // console.log('setting entities field:', field);
+
+      resourceEntities[field] = +control.value;      // console.log('set entities field:', field);
+    }
+    let useGold = resourceEntities['sgold'] > resourceEntities['slumber'];
+    let rv = (resourceEntities['srum'] <= 0) || (resourceEntities['rum'] <= 0) ||
+            !(useGold ? resourceEntities['sgold'] > 0 && resourceEntities['gold'] > 0 :
+                        resourceEntities['slumber'] > 0 && resourceEntities['lumber'] > 0);
+    // console.log('anyFormErrors returning:', rv);
+    return rv;
+  }
+  public onValueChanged(data?: any) : [INumberHash] {
+    let resourceEntities: [INumberHash] = [{gold: 0}];
+    if (!this.formModel) { return null; }
     const form = this.formModel;
     for (const field in this.formErrors) {
       if (field.length === 0) {
@@ -100,6 +157,10 @@ export class CalculateMaxComponent implements OnInit {
           }
         }
       }
+      else if (control) {
+      resourceEntities[field] = +control.value;
+      }
     }
+    return resourceEntities;
   }
 }
---
+++ src/utils/iDictionary.ts
@@ -0,0 +1,3 @@
+export interface INumberHash {
+ [id: string]: number
+} 
